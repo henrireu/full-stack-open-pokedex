ebba2ece5db93471af6f5ac734948237dc81585c
reijomo <95440863+reijomo@users.noreply.github.com>
2024-11-25 15:28:59 +0200

    juu

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   const match = useMatch('/pokemon/:name')
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
 
-  console.log('testi5')
+  console.log('testi6')
   if (isLoading) {
     return <LoadingSpinner />
   }
